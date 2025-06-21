1cdd4ed56ef6b59deb7ca88d33298ee39cc3360d
Jake Soenneker <jake@soenneker.com>
2025-06-21 09:47:13 -0500

    misc

--- test/Soenneker.Utils.Directory.Tests/DirectoryUtilTests.cs
+++ test/Soenneker.Utils.Directory.Tests/DirectoryUtilTests.cs
@@ -1,5 +1,3 @@
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using Soenneker.Utils.Directory.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
--- test/Soenneker.Utils.Directory.Tests/Fixture.cs
+++ test/Soenneker.Utils.Directory.Tests/Fixture.cs
@@ -1,4 +1,3 @@
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Soenneker.Fixtures.Unit;
@@ -23,7 +22,7 @@ public class Fixture : UnitFixture
             builder.AddSerilog(dispose: true);
         });
 
-        IConfiguration config = TestUtil.BuildConfig();
+        var config = TestUtil.BuildConfig();
         services.AddSingleton(config);
 
         services.AddDirectoryUtilAsScoped();
